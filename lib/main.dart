0da05ba42cb474fdbba5703c4e32bbce58e891b4
Leah <82816937+yLeah@users.noreply.github.com>
2023-08-13 01:27:50 -0400

    app icons and attempted round launcher

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mushroomiva'),
     );
   }
 }
