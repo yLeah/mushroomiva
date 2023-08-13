e653142012fba9f31bbae530395d57a14334eb2c
Leah <82816937+yLeah@users.noreply.github.com>
2023-08-13 07:58:05 -0400

    Encyclopedia finished

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ final List<Widget> pages = [
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        Image.asset('assets/e-chanterelle.jpg', scale: 0.5),
         const Text("Chanterelle", style: TextStyle(fontSize: 30)),
         const Text("Cantharellus cibarius",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
@@ -31,96 +31,102 @@ final List<Widget> pages = [
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        Image.asset('assets/e-kingtrumpet.png', scale: 0.5),
         const Text("King Trumpet Mushroom", style: TextStyle(fontSize: 30)),
         const Text("Pleurotus eryngii",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
         const Text(
-            "Originally native to the Mediterranean region, these mushrooms thrive in a variety of climates and are often grown indoors on a substrate made of straw or sawdust. They are known for their adaptability and can tolerate different temperatures, making them suitable for cultivation throughout the year. However, they are also found in the wild, primarily in grasslands and woodlands, growing in clusters or solitary form.",
+            "Originally native to the Mediterranean region, these mushrooms thrive in a variety of climates and are often grown indoors on a substrate made of straw or sawdust. However, they are also found in the wild, primarily in grasslands and woodlands, growing in clusters or solitary form.",
             style: TextStyle(fontSize: 18))
       ]))), //King Trumpet Mushroom
   Container(
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        const SizedBox(height: 10),
+        Image.asset('assets/e-morel.jpg',
+            height: 300, width: 200, fit: BoxFit.fill),
         const Text("True Morels", style: TextStyle(fontSize: 30)),
         const Text("Morchella spp.",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
         const Text(
-            "Morels are known to grow in diverse habitats, including forests, woodlands, grasslands, and even disturbed areas. They often emerge in the spring, following a period of rain and warm weather. True Morels are commonly found in North America, Europe, and parts of Asia. They have a mycorrhizal relationship with trees, forming a symbiotic association where the fungi and the host tree mutually benefit from each other.",
+            "Morels are known to grow in diverse habitats, including forests, woodlands, grasslands, and even disturbed areas. True Morels are commonly found in North America, Europe, and parts of Asia. They have a mycorrhizal relationship with trees, where the fungi and the host tree mutually benefit from each other.",
             style: TextStyle(fontSize: 18))
       ]))), //True Morels
   Container(
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        const SizedBox(height: 10),
+        Image.asset('assets/e-lionmane.jpg',
+            height: 350, width: 200, fit: BoxFit.fill),
         const Text("Lion's Mane", style: TextStyle(fontSize: 30)),
         const Text("Hericium erinaceus",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
         const Text(
-            "Lion's Mane mushrooms are typically found growing on decaying or dead hardwood trees, such as oak, beech, maple, or walnut. They are native to North America, Europe, and parts of Asia, but can also be cultivated indoors on substrates made from sawdust or wood chips. Cultivation methods have made Lion's Mane mushrooms more accessible and readily available to consumers.",
+            "Lion's Mane mushrooms are typically found growing on decaying or dead hardwood trees, such as oak, beech, maple, or walnut. They are native to North America, Europe, and parts of Asia.",
             style: TextStyle(fontSize: 18))
       ]))), //Lion's Mane
   Container(
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        Image.asset('assets/e-pennybun.jpg', scale: 0.5),
         const Text("Penny Bun", style: TextStyle(fontSize: 30)),
         const Text("Boletus edulis",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
         const Text(
-            "Penny Bun mushrooms are found in a wide range of habitats, including deciduous and coniferous forests, often growing in association with certain tree species like oak, beech, pine, or birch. They have a mycorrhizal relationship with these trees, forming a mutually beneficial partnership where the fungus aids in the tree's nutrient absorption while receiving carbohydrates in return. Penny Bun mushrooms can be found in various regions across Europe, North America, and Asia, and they are highly regarded in many culinary traditions around the world.",
+            "Penny Bun mushrooms are found in a wide range of habitats, including deciduous and coniferous forests, often growing in association with certain tree species like oak, beech, pine, or birch. They have a mycorrhizal relationship with these trees. Penny Bun mushrooms can be found in various regions across Europe, North America, and Asia.",
             style: TextStyle(fontSize: 18))
       ]))), //Penny Bun
   Container(
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        Image.asset('assets/e-hen.jpg', scale: 0.5),
         const Text("Hen-Of-The-Wood", style: TextStyle(fontSize: 30)),
         const Text("Grifola erondosa",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
         const Text(
-            "Hen-of-the-Wood mushrooms are typically found growing at the base of hardwood trees, such as oak, maple, and beech, in temperate forests of North America, Europe, and Asia. They are primarily a woodland species and often appear in the fall, although they can occasionally be found at other times of the year. These mushrooms form a mycorrhizal relationship with their host trees, exchanging nutrients and forming a symbiotic partnership.",
+            "Hen-of-the-Wood mushrooms are typically found growing at the base of hardwood trees, such as oak, maple, and beech, in temperate forests of North America, Europe, and Asia. They are primarily a woodland species and often appear in the fall, although they can occasionally be found at other times of the year.",
             style: TextStyle(fontSize: 18))
       ]))), //Hen-Of-The-Wood
   Container(
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        const SizedBox(height: 10),
+        Image.asset('assets/e-oyster.jpg',
+            height: 350, width: 250, fit: BoxFit.fill),
         const Text("Oyster Mushroom", style: TextStyle(fontSize: 30)),
         const Text("Pleurotus ostreatus",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
         const Text(
-            "Oyster mushrooms have a wide distribution and can be found growing naturally in various parts of the world. They are known to flourish on dead or decaying organic matter, particularly hardwood trees, although they can also grow on straw, sawdust, or agricultural waste. Oyster mushrooms are not mycorrhizal, meaning they do not form symbiotic relationships with host trees. This characteristic makes them relatively easy to cultivate and commercially produce.",
+            "Oyster mushrooms have a wide distribution and can be found growing naturally in various parts of the world. Oyster mushrooms are not mycorrhizal, meaning they do not form symbiotic relationships with host trees.",
             style: TextStyle(fontSize: 18))
       ]))), //Pleurotus Ostreatus
   Container(
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        Image.asset('assets/e-shiitake.jpg', scale: 0.5),
         const Text("Shiitake", style: TextStyle(fontSize: 30)),
         const Text("Lentinula edodes",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
         const Text(
-            "Shiitake mushrooms are native to East Asia and are particularly associated with Japanese and Chinese culinary traditions. While they can be found growing in the wild on decaying hardwood logs, they are also extensively cultivated on artificial substrates. Shiitake cultivation involves inoculating hardwood logs or sawdust blocks with the mushroom spawn, providing optimal conditions for growth, and controlling factors such as temperature, humidity, and light.",
+            "Shiitake mushrooms are native to East Asia and are particularly associated with Japanese and Chinese culinary traditions. While they can be found growing in the wild on decaying hardwood logs, they are also extensively cultivated on artificial substrates.",
             style: TextStyle(fontSize: 18))
       ]))), //Shiitake
   Container(
       color: Colors.white,
       child: Center(
           child: Column(children: [
-        Image.asset(''),
+        Image.asset('assets/e-portobello.jpg', scale: 0.5),
         const Text("Portobello", style: TextStyle(fontSize: 30)),
         const Text("Agaricus bisporus",
             style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)),
         const Text(
-            "Portobello mushrooms are typically cultivated rather than found in the wild. They are a cultivated variety of the common white button mushroom, selected for their larger size and more robust flavor. Portobellos are grown in a controlled environment, often on composted substrates, under specific conditions of temperature, humidity, and light. The cultivation process allows for consistent production and availability throughout the year.",
+            "Portobello mushrooms are typically cultivated rather than found in the wild. They are a cultivated variety of the common white button mushroom, selected for their larger size and more robust flavor.",
             style: TextStyle(fontSize: 18))
       ]))), //Agaricus Bisporus
   Container(color: globals.taupe), //Back Cover
